debe6bceefab15393bdfc9dad36789b099d8b4a3
maxim <panin-max@live.com>
2020-10-31 23:35:02 +0300

    no message

--- Atom.Culture.App.Data/Models/Person.cs
+++ Atom.Culture.App.Data/Models/Person.cs
@@ -9,7 +9,7 @@ namespace Atom.Culture.App.Data.Models
         public string Email { get; set; }
         public string BirthDate { get; set; }
         public List<Book> Books { get; set; }
-        public List<Recomendation> Recomendations { get; set; }
+        public List<Book> Recomendations { get; set; }
         public DateTime LastUpdate { get; set; }
     }
 }
